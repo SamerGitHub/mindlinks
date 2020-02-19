88c22d16a0a7b2cc8110ab26e9294cd18b9ce13a
Tuxette Chix <tuxette@chix.nathalievilla.org>
2020-02-19 16:00:02 +0100

    fatma

--- mindlinks/src/mindlinks/Etudiant.java
+++ mindlinks/src/mindlinks/Etudiant.java
@@ -1,9 +1,10 @@
 package mindlinks;
 
-public class Etudiant   {
+public class Etudiant extends Personne  {
 public int numCarteEtudiant;
-public Etudiant(int numCarteEtudiant)
+public Etudiant(String nom ,String prenom ,int age,int numCarteEtudiant)
 {
+	super(nom,prenom,age);
 	this.numCarteEtudiant=numCarteEtudiant ;
 	
 }
