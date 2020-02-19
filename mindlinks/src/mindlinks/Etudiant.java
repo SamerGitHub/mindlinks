f27c43cbfaf21facb71733d7effd82e32dbfdb73
HP <HP@DESKTOP-FF4BIFK>
2020-02-19 16:25:29 +0100

    nsalah

--- mindlinks/src/mindlinks/Etudiant.java
+++ mindlinks/src/mindlinks/Etudiant.java
@@ -1,10 +1,8 @@
 package mindlinks;
 
-<<<<<<< HEAD
 public class Etudiant    {
-=======
 public class Etudiant extends Personne  {
->>>>>>> branch 'master' of https://github.com/SamerGitHub/mindlinks.git
+
 public int numCarteEtudiant;
 public Etudiant(String nom ,String prenom ,int age,int numCarteEtudiant)
 {
