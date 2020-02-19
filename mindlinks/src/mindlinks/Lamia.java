3a6410a5d7a7fec8ad2ac97af8c24e0d0fc9df2e
Asus <Asus@192.168.1.9>
2020-02-19 16:33:27 +0100

    modufication in class Lamia

--- mindlinks/src/mindlinks/Lamia.java
+++ mindlinks/src/mindlinks/Lamia.java
@@ -1,5 +1,20 @@
 package mindlinks;
 
-public class Lamia {
+public class Lamia extends Personne {
+	public String etat ;
+	 public Lamia (String nom , String prenom , int age , String etat)
+	 {
+		 super(nom,prenom,age);
+		 this.etat=etat;
+	 }
+	 
+	public String getEtat()
+	{
+		return etat;
+	}
+	public void setEtat(String e)
+	{
+		etat=e;
+	}
 
 }
