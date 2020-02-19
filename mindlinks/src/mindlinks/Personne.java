be1fb15b09232137ecc9a5dbf9a6dcd806fbc75d
HP <HP@DESKTOP-FF4BIFK>
2020-02-19 15:25:37 +0100

    Take

--- mindlinks/src/mindlinks/Personne.java
+++ mindlinks/src/mindlinks/Personne.java
@@ -4,6 +4,11 @@ public class Personne {
 	 public String nom ;
 	 public String prenom;
 	 public int age ; 
-	 
+	 public Personne(String nom , String prenom , int age)
+	 {
+		 this.nom=nom;
+		 this.age=age;
+		 this.prenom=prenom;
+	 }
 
 }
