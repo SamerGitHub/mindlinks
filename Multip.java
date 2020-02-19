088df1506823b79b4eb47ce6c050fccbafae7789
Tuxette Chix <tuxette@chix.nathalievilla.org>
2020-02-19 16:52:56 +0100

    add by chayma

---
+++ Multip.java
@@ -0,0 +1,14 @@
+
+public class Multip {
+	int a ;
+	int b ; 
+	public Multiplication (int a , int b ) {
+		this.a = a ;
+		this.b = b ;
+	}
+	public int mult(int a , int b ) {
+		return a*b ; 
+	}
+
+}
+
