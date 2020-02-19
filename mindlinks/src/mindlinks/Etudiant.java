bf08ac86497fc8b24638c4579028a69947ea21b8
hp <hp@192.168.1.17>
2020-02-19 16:44:00 +0100

    update

--- mindlinks/src/mindlinks/Etudiant.java
+++ mindlinks/src/mindlinks/Etudiant.java
@@ -1,10 +1,5 @@
 package mindlinks;
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> branch 'master' of https://github.com/SamerGitHub/mindlinks
 public class Etudiant extends Personne  {
 public int numCarteEtudiant;
 public Etudiant(String nom ,String prenom ,int age,int numCarteEtudiant)
