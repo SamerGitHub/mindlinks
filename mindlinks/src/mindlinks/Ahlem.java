56cdc27c68db8c4e4a500268febebf73a6cbee1a
hp <hp@192.168.1.17>
2020-02-19 16:16:30 +0100

    add class ahlem

---
+++ mindlinks/src/mindlinks/Ahlem.java
@@ -0,0 +1,13 @@
+package mindlinks;
+
+public class Ahlem {
+	String nom , prenom; 
+	int age ; 
+	public Ahlem(String nom ,String prenom,int age ) {
+		this.nom = nom ;
+		this.prenom =prenom;
+		this.age = age ;
+		
+	}
+	
+}
