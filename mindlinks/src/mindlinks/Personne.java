3c08b4cf27a37edba7586517d6802ca5f65bea08
Admin <Admin@192.168.1.11>
2020-02-19 16:55:27 +0100

    age

--- mindlinks/src/mindlinks/Personne.java
+++ mindlinks/src/mindlinks/Personne.java
@@ -3,12 +3,20 @@ package mindlinks;
 public class Personne {
 	 public String nom ;
 	 public String prenom;
-	 public int age ; 
+	 public int j ;
+	public int m ;
+	public int ann ; ; 
 
-	 public Personne(String nom , String prenom , int age)
+	 public Personne(String nom , String prenom , int j, int m ,int ann)
 	 {
 		 this.nom=nom;
-		 this.age=age;
+			{ this.j=j;
+			this.m=m;
+			this.ann=ann;
 		 this.prenom=prenom;
 	 }
+			public int age (int ann ) {
+				return 2020-ann;
+				
+			}
 }
