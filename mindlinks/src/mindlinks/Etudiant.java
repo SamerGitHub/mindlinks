454472184d8270b7f2d4ea1e601b74cb3ac4c1fa
Asus <Asus@192.168.1.9>
2020-02-19 16:38:35 +0100

    modification

--- mindlinks/src/mindlinks/Etudiant.java
+++ mindlinks/src/mindlinks/Etudiant.java
@@ -1,7 +1,5 @@
 package mindlinks;
 
-public class Etudiant    {
-
 public class Etudiant extends Personne  {
 public int numCarteEtudiant;
 public Etudiant(String nom ,String prenom ,int age,int numCarteEtudiant)
